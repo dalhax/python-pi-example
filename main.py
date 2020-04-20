6a5eb57cc9c45849b4e81498a4506220b9a6593f
dalhax <siposdani93@gmail.com>
2020-04-20 15:38:13 +0200

    Resolved the conflicts

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 #!usr/bin/python3
 
 def main(msg):
-<<<<<<< HEAD
+
 	// Adding a second comment to this file 
-=======
 	// Print a message
->>>>>>> refs/remotes/origin/master
+
     print(msg)
 
 main("Hello People")
